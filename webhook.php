1c0d02b236d208f5460af803e082fd75fcd96de6
itsinayat <inayat.rdec@gmail.com>
2025-01-05 14:25:19 +0400

    fix

--- webhook.php
+++ webhook.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $apiKey = '7333937557:AAHaU4M025dD5dUNikPEyC4vWLFmZFmygxM'; // Your Telegram bot API key
 $apiUrl = "https://api.telegram.org/bot$apiKey/";
 
